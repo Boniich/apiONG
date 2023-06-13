a04b3b257ad8e9b18a5581c9fd033d34cb45c68d
boniich <ezequieldbo25@gmail.com>
2023-06-13 10:31:11 -0300

    feat: add model, migration, controller and seeder for member table

---
+++ database/migrations/2023_06_13_132346_create_members_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('members', function (Blueprint $table) {
+            $table->id();
+            $table->string('full_name', 100);
+            $table->string('description', 150);
+            $table->string('image', 70);
+            $table->string('facebook_url', 150);
+            $table->string('linkedin_url', 150);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('members');
+    }
+};
