065258ee096e14899ffccec556136162ad41a253
boniich <ezequieldbo25@gmail.com>
2023-06-14 15:46:03 -0300

    feat: add three new function to reduce lines of code

--- app/Helpers/responseFunction.php
+++ app/Helpers/responseFunction.php
@@ -15,3 +15,25 @@ if (!function_exists('errorResponse')) {
         return ["success" => false, "error" => $errorMsg];
     }
 }
+
+if (!function_exists('okResponse200')) {
+    function okResponse200($data, string $message)
+    {
+        return response()->json(successResponse($data, $message), 200);
+    }
+}
+
+if (!function_exists('notFoundData404')) {
+    function notFoundData404(string $message)
+    {
+        return response()->json(errorResponse($message), 404);
+    }
+}
+
+if (!function_exists('badRequestResponse400')) {
+
+    function badRequestResponse400()
+    {
+        return response()->json(errorResponse("Bad request"), 400);
+    }
+}
