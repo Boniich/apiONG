3b76f75e0660ea207c02a0e74dfca639adf1db91
boniich <ezequieldbo25@gmail.com>
2023-06-14 16:07:48 -0300

    feat: add seeder about contact

--- database/seeders/ContactSeeder.php
+++ database/seeders/ContactSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Contact;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,14 @@ class ContactSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $contact = new Contact;
+
+        $contact->id = 1;
+        $contact->name = "Carlos Perez";
+        $contact->email = "Carlosperez@gmail.com";
+        $contact->phone = "111111110";
+        $contact->message = "Mensajito de carlos";
+
+        $contact->save();
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(MemberSeeder::class);
         $this->call(OrganizationSeeder::class);
+        $this->call(ContactSeeder::class);
     }
 }
