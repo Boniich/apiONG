987a46c83a14475e2248cdfeebb396d45e71d820
boniich <ezequieldbo25@gmail.com>
2023-06-15 11:19:30 -0300

    feat: make possible that test make a request with image

--- tests/Feature/OrganizationTest.php
+++ tests/Feature/OrganizationTest.php
@@ -2,8 +2,11 @@
 
 namespace Tests\Feature;
 
+use App\Models\Organization;
+use Database\Seeders\OrganizationSeeder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Http\UploadedFile;
 use Tests\CreatesApplication;
 use Tests\TestCase;
 
@@ -14,17 +17,18 @@ class OrganizationTest extends TestCase
 
     public function test_show_unique_register_of_organization_successfully(): void
     {
-        $this->seed();
+        $this->seed(OrganizationSeeder::class);
         $response = $this->get('api/organization');
         $response->assertStatus(200);
     }
 
     public function test_update__unique_register_of_organization_successfully(): void
     {
-        $this->seed();
-        $response = $this->put('api/organization', [
+        $this->seed(OrganizationSeeder::class);
+        $this->put('api/organization/', [
+            'id' => 1,
             'name' => 'Actualizado',
-            'logo' => 'Actualizado',
+            'logo' => UploadedFile::fake()->image("image.png"),
             'short_description' => 'Actualizado',
             'long_description' => 'Actualizado',
             'welcome_text' => 'Actualizado',
@@ -35,14 +39,15 @@ class OrganizationTest extends TestCase
             'linkedin_url' => 'Actualizado',
             'instagram_url' => 'Actualizado',
             'twitter_url' => 'Actualizado',
-        ]);
+        ])->assertStatus(200);
 
-        $response->assertStatus(200);
+        $imageCreated = Organization::find(1);
+        deleteLoadedImage($imageCreated->logo);
     }
 
     public function test_bad_request_at_update_organization_data_successfully(): void
     {
-        $this->seed();
+        $this->seed(OrganizationSeeder::class);
         $response = $this->put('api/organization', [
             'name' => 'Actualizado',
             'logo' => 'Actualizado',
