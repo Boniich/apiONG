4faad1af07f7c4c7df83d05f6c7a52d66a66bc83
boniich <ezequieldbo25@gmail.com>
2023-06-15 14:12:04 -0300

    feat: add seeder to project

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,5 +17,6 @@ class DatabaseSeeder extends Seeder
         $this->call(ContactSeeder::class);
         $this->call(TestimonialSeeder::class);
         $this->call(SocialMediaItemSeeder::class);
+        $this->call(ProjectSeeder::class);
     }
 }
--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Project;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,14 @@ class ProjectSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $project = new Project;
+
+        $project->id = 1;
+        $project->name = "Project 1";
+        $project->description = "Description of project 1";
+        $project->image = upLoadImageToSeeders("projects");
+        $project->due_date = "2023";
+
+        $project->save();
     }
 }
