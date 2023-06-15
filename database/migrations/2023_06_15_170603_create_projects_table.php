6222b5b30cf84909587ae4d22aa43621f7e80575
boniich <ezequieldbo25@gmail.com>
2023-06-15 14:11:50 -0300

    feat: add table to project

--- database/migrations/2023_06_15_170603_create_projects_table.php
+++ database/migrations/2023_06_15_170603_create_projects_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 50);
+            $table->string('description', 200);
+            $table->string('image', 70);
+            $table->string('due_date', 50);
             $table->timestamps();
         });
     }
