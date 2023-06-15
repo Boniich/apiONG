04654f1523b7a34af2ba003c173884584abaa0e5
boniich <ezequieldbo25@gmail.com>
2023-06-15 12:04:30 -0300

    feat: add seeders to testimonial

--- database/seeders/TestimonialSeeder.php
+++ database/seeders/TestimonialSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Testimonial;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,12 @@ class TestimonialSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $testimonial = new Testimonial;
+
+        $testimonial->name = "Testimonio 1";
+        $testimonial->image = upLoadImageToSeeders("testimonial");
+        $testimonial->description = "Descripcion del testimonio 1";
+
+        $testimonial->save();
     }
 }
