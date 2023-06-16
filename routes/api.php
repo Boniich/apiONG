a0f510ccc30418ebe3e80e7fd068a7c86a85515b
boniich <ezequieldbo25@gmail.com>
2023-06-16 18:56:06 -0300

    feat: add patch route to users

--- routes/api.php
+++ routes/api.php
@@ -70,6 +70,7 @@ Route::get('users', [UserController::class, 'index']);
 Route::get('users/{id}', [UserController::class, 'show']);
 Route::post('users', [UserController::class, 'store']);
 Route::put('users/{id}', [UserController::class, 'update']);
+Route::patch('users/{id}', [UserController::class, 'update']);
 Route::delete('users/{id}', [UserController::class, 'delete']);
 
 Route::fallback(function () {
