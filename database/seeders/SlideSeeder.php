a21b0748fe86491504e978a8a39a3724a47d2541
boniich <ezequieldbo25@gmail.com>
2023-06-17 11:00:29 -0300

    feat: add seeder to slider

--- database/seeders/SlideSeeder.php
+++ database/seeders/SlideSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Slide;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,14 @@ class SlideSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $slider = new Slide();
+
+        $slider->id = 1;
+        $slider->name = "Slider 1";
+        $slider->description = "First slider";
+        $slider->image = upLoadImageToSeeders("slider");
+        $slider->order = 1;
+
+        $slider->save();
     }
 }
