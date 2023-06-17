647467d647fae36618fcad0c69f1b69de03c5ad6
boniich <ezequieldbo25@gmail.com>
2023-06-17 12:28:43 -0300

    feat: add filds to activies table

--- database/migrations/2023_06_17_151832_create_activities_table.php
+++ database/migrations/2023_06_17_151832_create_activities_table.php
@@ -13,6 +13,17 @@ return new class extends Migration
     {
         Schema::create('activities', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 70);
+            $table->string('slug', 70)->nullable();
+            $table->string('description', 200);
+            $table->string('image', 70);
+
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+
+            $table->unsignedBigInteger('category_id')->nullable();
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+
             $table->timestamps();
         });
     }
