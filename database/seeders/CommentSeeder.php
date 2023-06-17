05155886fca1898d95d048120cd3355f4bcdc133
boniich <ezequieldbo25@gmail.com>
2023-06-17 15:26:06 -0300

    feat: add seeder to comment

--- database/seeders/CommentSeeder.php
+++ database/seeders/CommentSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Comment;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,12 @@ class CommentSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $comment = new Comment;
+
+        $comment->id = 1;
+        $comment->text = "Comment 1";
+        $comment->visible = true;
+
+        $comment->save();
     }
 }
