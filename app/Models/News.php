78f46fe14d5ef3cf570eb477d55d6e4cec02b2c9
boniich <ezequieldbo25@gmail.com>
2023-06-17 15:00:46 -0300

    feat: add relationship at news model

--- app/Models/News.php
+++ app/Models/News.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class News extends Model
 {
     use HasFactory;
+
+
+    public function users()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function categories()
+    {
+        return $this->belongsTo(Category::class, 'category_id');
+    }
 }
