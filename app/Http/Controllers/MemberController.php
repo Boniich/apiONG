2e230d6cd0498a73d4a167a2edf3edfa84bcd220
boniich <ezequieldbo25@gmail.com>
2023-06-21 14:04:26 -0300

    doct: add documentacion about member methods

--- app/Http/Controllers/MemberController.php
+++ app/Http/Controllers/MemberController.php
@@ -11,6 +11,39 @@ class MemberController extends Controller
 
     private string $notFoundMsg = "Member not found";
 
+
+    /**
+     * Display a listing of members.
+     * @return \Illuminate\Http\Response
+     *
+     * @OA\Get(
+     *     path="/api/members",
+     *     tags={"Members"},
+     *     summary="Display a listing of members.",
+     *     @OA\Response(
+     *         response=200,
+     *         description="Members retrived succesffully",
+     *         content={
+     *                  @OA\MediaType(
+     *                      mediaType="application/json",
+     *                      example={
+     *                          "id": 1,
+     *                          "full_name": "Pedro",
+     *                          "description": "Pedro is important member",
+     *                          "image": "image.png",
+     *                          "facebook_url": "facebook",
+     *                          "linkedin_url": "linkedin",
+     *                          "created_at": "2023-06-17T18:25:27.000000Z",
+     *                          "updated_at": "2023-06-17T18:25:27.000000Z"
+     *                      })},
+     *     ),
+     *     @OA\Response(
+     *         response="default",
+     *         description="An error ocurred"
+     *     )
+     * ) 
+     */
+
     public function index()
     {
         try {
@@ -21,6 +54,46 @@ class MemberController extends Controller
             return response()->json(errorResponse("An Error occurred"));
         }
     }
+
+
+    /**
+     * Display a member by id.
+     * @return \Illuminate\Http\Response
+     *
+     * @OA\Get(
+     *     path="/api/members/{id}",
+     *     tags={"Members"},
+     *     summary="Display a member.",
+     *     @OA\Parameter(
+     *          description="id of member",
+     *          in="path",
+     *          name="id",
+     *          required=true,
+     *          @OA\Schema(type="string"),
+     *      ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Member retrived succesffully",
+     *         content={
+     *                  @OA\MediaType(
+     *                      mediaType="application/json",
+     *                      example={
+     *                          "id": 1,
+     *                          "full_name": "Pedro",
+     *                          "description": "Pedro is important member",
+     *                          "image": "image.png",
+     *                          "facebook_url": "facebook",
+     *                          "linkedin_url": "linkedin",
+     *                          "created_at": "2023-06-17T18:25:27.000000Z",
+     *                          "updated_at": "2023-06-17T18:25:27.000000Z"
+     *                      })},
+     *     ),
+     *     @OA\Response(
+     *         response=404,
+     *         description="Member not found"
+     *     )
+     * ) 
+     */
 
     public function show($id)
     {
@@ -34,6 +107,41 @@ class MemberController extends Controller
             return response()->json(errorResponse("An Error occurred"));
         }
     }
+
+    /**
+     * Create a new member.
+     * Note: This endpoint does not work in swagger cause is not possible upload an image here.
+     * @OA\Post(
+     *      path="/api/members",
+     *      summary="Create a new member",
+     *      tags={"Members"},
+     * 
+     *      @OA\RequestBody(
+     *          required=true,
+     *          @OA\JsonContent(
+     *          required={"full_name", "description", "image", "facebook_url", "linkedin_url"},
+     *          @OA\Property(property="id", type="integer", format="string"),
+     *          @OA\Property(property="full_name", type="string", format="string"),
+     *          @OA\Property(property="description", type="string", format="string" ),
+     *          @OA\Property(property="image", type="string", format="string"),
+     *          @OA\Property(property="facebook_url", type="string", format="string"),
+     *          @OA\Property(property="linkedin_url", type="string", format="string"),
+     *                    ),
+     *              ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="Member created successfully"  
+     *      ),
+     *      @OA\Response(
+     *          response=400,
+     *          description="bad request"  
+     *      ),
+     *      @OA\Response(
+     *          response="default",
+     *          description="An error has occurred"
+     *      )
+     * )
+     */
 
     public function store(Request $request)
     {
@@ -61,6 +169,46 @@ class MemberController extends Controller
             return response()->json(errorResponse("Bad Request"), 400);
         }
     }
+
+    /**
+     * Update a member
+     * Note: This endpoint does not work in swagger if you add the field "image" cause is not possible upload an image here.
+     * @OA\Put(
+     *      path="/api/members/{id}",
+     *      summary="Update an member",
+     *      tags={"Members"},
+     *      @OA\Parameter(
+     *          description="id of member",
+     *          in="path",
+     *          name="id",
+     *          required=true,
+     *          @OA\Schema(type="string"),
+     *      ),
+     *      @OA\RequestBody(
+     *          required=true,
+     *          @OA\JsonContent(
+     *          @OA\Property(property="id", type="integer", format="string"),
+     *          @OA\Property(property="full_name", type="string", format="string"),
+     *          @OA\Property(property="description", type="string", format="string" ),
+     *          @OA\Property(property="image", type="string", format="string"),
+     *          @OA\Property(property="facebook_url", type="string", format="string"),
+     *          @OA\Property(property="linkedin_url", type="string", format="string"),
+     *                    ),
+     *              ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="Member updated successfully"  
+     *      ),
+     *      @OA\Response(
+     *          response=404,
+     *          description="Member not found"
+     *      ),
+     *      @OA\Response(
+     *          response=400,
+     *          description="bad request"  
+     *      ),
+     * )
+     */
 
     public function update(Request $request, $id)
     {
@@ -108,6 +256,35 @@ class MemberController extends Controller
             return response()->json(errorResponse("Bad Request"), 400);
         }
     }
+
+    /**
+     * Delete a member
+     * @OA\Delete(
+     *      path="/api/members/{id}",
+     *      summary="Delete a member",
+     *      tags={"Members"},
+     * 
+     *       @OA\Parameter(
+     *          description="id of member",
+     *          in="path",
+     *          name="id",
+     *          required=true,
+     *          @OA\Schema(type="string"),
+     *      ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="Member delete succesfully"  
+     *      ),
+     *      @OA\Response(
+     *          response=404,
+     *          description="Member not found"  
+     *      ),
+     *      @OA\Response(
+     *          response="default",
+     *          description="An Error ocurred"
+     *      )
+     * )
+     */
 
     public function delete($id)
     {
