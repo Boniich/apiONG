27eafa315aee0a5d34bef14ba4a36ba59e30f166
boniich <ezequieldbo25@gmail.com>
2023-06-21 16:54:49 -0300

    doc: add slide doc

--- app/Http/Controllers/SlideController.php
+++ app/Http/Controllers/SlideController.php
@@ -11,6 +11,39 @@ class SlideController extends Controller
 {
     private string $notFoundMsg = "Slide not found";
 
+
+    /**
+     * Display a listing of Slides.
+     * @return \Illuminate\Http\Response
+     *
+     * @OA\Get(
+     *     path="/api/slides",
+     *     tags={"Slides"},
+     *     summary="Display a listing of slides.",
+     *     @OA\Response(
+     *         response=200,
+     *         description="Slides retrived succesffully",
+     *         content={
+     *                  @OA\MediaType(
+     *                      mediaType="application/json",
+     *                      example={
+     *                          "id": 1,
+     *                          "name": "slide name",
+     *                          "description": "this is a slide",
+     *                          "image": "1687376560.jpg",
+     *                          "order": 5,
+     *                          "user_id": 5,
+     *                          "created_at": "2023-06-17T18:25:27.000000Z",
+     *                          "updated_at": "2023-06-21T19:42:40.000000Z"
+     *                      })},
+     *     ),
+     *     @OA\Response(
+     *         response="default",
+     *         description="An error ocurred"
+     *     )
+     * ) 
+     */
+
     public function index()
     {
         try {
@@ -21,6 +54,45 @@ class SlideController extends Controller
             return anErrorOcurred();
         }
     }
+
+    /**
+     * Display a slide by id.
+     * @return \Illuminate\Http\Response
+     *
+     * @OA\Get(
+     *     path="/api/slides/{id}",
+     *     tags={"Slides"},
+     *     summary="Display a slide.",
+     *     @OA\Parameter(
+     *          description="id of slide",
+     *          in="path",
+     *          name="id",
+     *          required=true,
+     *          @OA\Schema(type="string"),
+     *      ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Slide retrived succesffully",
+     *         content={
+     *                  @OA\MediaType(
+     *                      mediaType="application/json",
+     *                      example={
+     *                          "id": 1,
+     *                          "name": "slide name",
+     *                          "description": "this is a slide",
+     *                          "image": "1687376560.jpg",
+     *                          "order": 5,
+     *                          "user_id": 5,
+     *                          "created_at": "2023-06-17T18:25:27.000000Z",
+     *                          "updated_at": "2023-06-21T19:42:40.000000Z"
+     *                      })},
+     *     ),
+     *     @OA\Response(
+     *         response=404,
+     *         description="Slide not found"
+     *     )
+     * ) 
+     */
 
     public function show($id)
     {
@@ -35,6 +107,40 @@ class SlideController extends Controller
         }
     }
 
+    /**
+     * Create a new slide.
+     * Note: This endpoint does not work in swagger cause is not possible upload an image here.
+     * @OA\Post(
+     *      path="/api/slides",
+     *      summary="Create a new slide",
+     *      tags={"Slides"},
+     * 
+     *      @OA\RequestBody(
+     *          required=true,
+     *          @OA\JsonContent(
+     *          required={"name","description","image","order"},
+     *          @OA\Property(property="id", type="integer", format="string"),
+     *          @OA\Property(property="name", type="string", format="string"),
+     *          @OA\Property(property="description", type="string", format="string" ),
+     *          @OA\Property(property="image", type="string", format="string"),
+     *          @OA\Property(property="order", type="integer", format="string"),
+     *          @OA\Property(property="user_id", type="integer", format="string"),
+     *                    ),
+     *              ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="Slide created successfully"  
+     *      ),
+     *      @OA\Response(
+     *          response=400,
+     *          description="bad request"  
+     *      ),
+     *      @OA\Response(
+     *          response="default",
+     *          description="An error has occurred"
+     *      )
+     * )
+     */
     public function store(Request $request)
     {
         try {
@@ -70,6 +176,46 @@ class SlideController extends Controller
             return badRequestResponse400();
         }
     }
+
+    /**
+     * Update a slide
+     * Note: This endpoint does not work in swagger if you add the field "image" cause is not possible upload an image here.
+     * @OA\Put(
+     *      path="/api/slides/{id}",
+     *      summary="Update a slide",
+     *      tags={"Slides"},
+     *      @OA\Parameter(
+     *          description="id of slide",
+     *          in="path",
+     *          name="id",
+     *          required=true,
+     *          @OA\Schema(type="string"),
+     *      ),
+     *      @OA\RequestBody(
+     *          required=true,
+     *          @OA\JsonContent(
+     *          @OA\Property(property="id", type="integer", format="string"),
+     *          @OA\Property(property="name", type="string", format="string"),
+     *          @OA\Property(property="description", type="string", format="string"),
+     *          @OA\Property(property="image", type="string", format="string"),
+     *          @OA\Property(property="order", type="integer", format="string"),
+     *          @OA\Property(property="user_id", type="integer", format="string"),
+     *                    ),
+     *              ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="Slide updated successfully"  
+     *      ),
+     *      @OA\Response(
+     *          response=404,
+     *          description="Slide not found"
+     *      ),
+     *      @OA\Response(
+     *          response=400,
+     *          description="Bad request"  
+     *      ),
+     * )
+     */
 
     public function update(Request $request, $id)
     {
@@ -120,6 +266,36 @@ class SlideController extends Controller
             return badRequestResponse400();
         }
     }
+
+    /**
+     * Delete a slide
+     * @OA\Delete(
+     *      path="/api/slides/{id}",
+     *      summary="Delete a slide",
+     *      tags={"Slides"},
+     * 
+     *       @OA\Parameter(
+     *          description="id of slide",
+     *          in="path",
+     *          name="id",
+     *          required=true,
+     *          @OA\Schema(type="string"),
+     *      ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="Slide delete succesfully"  
+     *      ),
+     *      @OA\Response(
+     *          response=404,
+     *          description="Slide not found"  
+     *      ),
+     *      @OA\Response(
+     *          response="default",
+     *          description="An Error ocurred"
+     *      )
+     * )
+     */
+
     public function delete($id)
     {
         try {
