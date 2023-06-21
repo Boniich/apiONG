f3643b08fd530a0dc6c09e76d4ebd4f65ffe56c8
boniich <ezequieldbo25@gmail.com>
2023-06-21 16:36:08 -0300

    doc: add doc about roles

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -11,6 +11,35 @@ class RoleController extends Controller
 
     private string $notFoundMsg = "Role not found";
 
+    /**
+     * Display a listing of roles.
+     * @return \Illuminate\Http\Response
+     *
+     * @OA\Get(
+     *     path="/api/roles",
+     *     tags={"Roles"},
+     *     summary="Display a listing of roles.",
+     *     @OA\Response(
+     *         response=200,
+     *         description="Roles retrived succesffully",
+     *         content={
+     *                  @OA\MediaType(
+     *                      mediaType="application/json",
+     *                      example={
+     *                          "id": 1,
+     *                          "name": "Admin",
+     *                          "guard_name": "web",
+     *                          "created_at": "2023-06-17T18:25:26.000000Z",
+     *                          "updated_at": "2023-06-17T18:25:26.000000Z"
+     *                      })},
+     *     ),
+     *     @OA\Response(
+     *         response="default",
+     *         description="An error ocurred"
+     *     )
+     * ) 
+     */
+
     public function index()
     {
         try {
@@ -21,6 +50,42 @@ class RoleController extends Controller
             return anErrorOcurred();
         }
     }
+
+    /**
+     * Display an contact by id.
+     * @return \Illuminate\Http\Response
+     *
+     * @OA\Get(
+     *     path="/api/roles/{id}",
+     *     tags={"Roles"},
+     *     summary="Display a rol",
+     *     @OA\Parameter(
+     *          description="id of rol",
+     *          in="path",
+     *          name="id",
+     *          required=true,
+     *          @OA\Schema(type="string"),
+     *      ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Rol retrived succesffully",
+     *         content={
+     *                  @OA\MediaType(
+     *                      mediaType="application/json",
+     *                      example={
+     *                          "id": 1,
+     *                          "name": "Admin",
+     *                          "guard_name": "web",
+     *                          "created_at": "2023-06-17T18:25:26.000000Z",
+     *                          "updated_at": "2023-06-17T18:25:26.000000Z"
+     *                      })},
+     *     ),
+     *     @OA\Response(
+     *         response=404,
+     *         description="Contact not found"
+     *     )
+     * ) 
+     */
 
     public function show($id)
     {
@@ -34,6 +99,41 @@ class RoleController extends Controller
             return anErrorOcurred();
         }
     }
+
+    /**
+     * Update a role
+     * @OA\Put(
+     *      path="/api/roles/{id}",
+     *      summary="Update a role",
+     *      tags={"Roles"},
+     *      @OA\Parameter(
+     *          description="id of role",
+     *          in="path",
+     *          name="id",
+     *          required=true,
+     *          @OA\Schema(type="string"),
+     *      ),
+     *      @OA\RequestBody(
+     *          required=true,
+     *          @OA\JsonContent(
+     *          required={"name"},
+     *          @OA\Property(property="name", type="string", format="string"),
+     *                    ),
+     *              ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="Role updated successfully"  
+     *      ),
+     *      @OA\Response(
+     *          response=404,
+     *          description="Role not found"
+     *      ),
+     *      @OA\Response(
+     *          response=400,
+     *          description="bad request"  
+     *      ),
+     * )
+     */
 
     public function update(Request $request, $id)
     {
