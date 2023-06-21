720f3960b6a742d3e99ddcaac1a9218ec1313327
boniich <ezequieldbo25@gmail.com>
2023-06-21 15:52:27 -0300

    doc: add doc to organization methods

--- app/Http/Controllers/OrganizationController.php
+++ app/Http/Controllers/OrganizationController.php
@@ -11,6 +11,45 @@ class OrganizationController extends Controller
 
     private string $notFoundMsg = "Organization not found";
 
+
+    /**
+     * Display a Organization
+     * @return \Illuminate\Http\Response
+     * @OA\Get(
+     *     path="/api/organization",
+     *     tags={"Organization"},
+     *     summary="Display a organization.",
+     *     @OA\Response(
+     *         response=200,
+     *         description="Organization retrived succesffully",
+     *         content={
+     *                  @OA\MediaType(
+     *                      mediaType="application/json",
+     *                      example={
+     *                          "id": 1,
+     *                          "name": "new org",
+     *                          "logo": "image.png",
+     *                          "short_description": "short description",
+     *                          "long_description": "long description",
+     *                          "welcome_text": "welcome text",
+     *                          "address": "Calle falsa 123",
+     *                          "phone": "00000",
+     *                          "cell_phone": "110000",
+     *                          "facebook_url": "face",
+     *                          "linkedin_url": "linkedin",
+     *                          "instagram_url": "insta",
+     *                          "twitter_url": "twitter",
+     *                          "created_at": "2023-06-17T18:25:26.000000Z",
+     *                          "updated_at": "2023-06-21T18:44:12.000000Z"
+     *                      })},
+     *     ),
+     *     @OA\Response(
+     *         response="default",
+     *         description="An error ocurred"
+     *     )
+     * ) 
+     */
+
     public function index()
     {
         try {
@@ -21,6 +60,41 @@ class OrganizationController extends Controller
             return response()->json(errorResponse("An Error occurred"), 400);
         }
     }
+
+    /**
+     * Update a organization data
+     * Note: This endpoint does not work in swagger if you add the field "image" cause is not possible upload an image here.
+     * @OA\Put(
+     *      path="/api/organization",
+     *      summary="Update organization",
+     *      tags={"Organization"},
+     *      @OA\RequestBody(
+     *          required=true,
+     *          @OA\JsonContent(
+     *          @OA\Property(property="id", type="integer", format="string"),
+     *          @OA\Property(property="name", type="string", format="string"),
+     *          @OA\Property(property="log", type="string", format="string" ),
+     *          @OA\Property(property="short_description", type="string", format="string"),
+     *          @OA\Property(property="long_description", type="string", format="string"),
+     *          @OA\Property(property="welcome_text", type="string", format="string"),
+     *          @OA\Property(property="address", type="string", format="string"),
+     *          @OA\Property(property="phone", type="string", format="string"),
+     *          @OA\Property(property="cell_phone", type="string", format="string"),
+     *          @OA\Property(property="facebook_url", type="string", format="string"),
+     *          @OA\Property(property="instagram_url", type="string", format="string"),
+     *          @OA\Property(property="twitter_url", type="string", format="string"),
+     *                    ),
+     *              ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="Organization updated successfully"  
+     *      ),
+     *      @OA\Response(
+     *          response=400,
+     *          description="bad request"  
+     *      ),
+     * )
+     */
 
     public function update(Request $request)
     {
