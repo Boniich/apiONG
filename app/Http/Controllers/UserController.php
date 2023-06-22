7bb450a6c8c5e862819ed88f3da571d027ccb83b
boniich <ezequieldbo25@gmail.com>
2023-06-21 21:27:52 -0300

    doc: add user doc

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -14,6 +14,54 @@ class UserController extends Controller
 
     private string  $notFoundMsg = "User not found";
 
+    /**
+     * Display a listing of users.
+     * @return \Illuminate\Http\Response
+     *
+     * @OA\Get(
+     *     path="/api/users/",
+     *     tags={"Users"},
+     *     summary="Display a listing of users.",
+     *     @OA\Parameter(
+     *          description="Search a term",
+     *          in="query",
+     *          name="search",
+     *          required=false,
+     *          @OA\Schema(type="string"),
+     *      ),
+     *      @OA\Parameter(
+     *          description="Limit of entries retrived",
+     *          in="query",
+     *          name="limit",
+     *          required=false,
+     *          @OA\Schema(type="string"),
+     *      ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Users retrived succesffully",
+     *         content={
+     *                  @OA\MediaType(
+     *                      mediaType="application/json",
+     *                      example={
+     *                          "id": 1,
+     *                          "name": "Prof. Javier Klocko",
+     *                          "email": "kuvalis.benedict@gmail.com",
+     *                          "email_verified_at": "2023-06-17T18:25:26.000000Z",
+     *                          "latitude": 0,
+     *                          "longitude": 0,
+     *                          "address": "1353 Dexter Motorway Suite 928",
+     *                          "profile_image": "user image",
+     *                          "created_at": "2023-06-17T18:25:26.000000Z",
+     *                          "updated_at": "2023-06-17T18:25:26.000000Z",
+     *                      })},
+     *     ),
+     *     @OA\Response(
+     *         response="default",
+     *         description="An error ocurred"
+     *     )
+     * ) 
+     */
+
     public function index(Request $request)
     {
         try {
@@ -40,6 +88,47 @@ class UserController extends Controller
         }
     }
 
+    /**
+     * Display a user by id.
+     * @return \Illuminate\Http\Response
+     *
+     * @OA\Get(
+     *     path="/api/users/{id}",
+     *     tags={"Users"},
+     *     summary="Display a user.",
+     *     @OA\Parameter(
+     *          description="id of user",
+     *          in="path",
+     *          name="id",
+     *          required=true,
+     *          @OA\Schema(type="string"),
+     *      ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="User retrived succesffully",
+     *         content={
+     *                  @OA\MediaType(
+     *                      mediaType="application/json",
+     *                      example={
+     *                          "id": 1,
+     *                          "name": "Prof. Javier Klocko",
+     *                          "email": "kuvalis.benedict@gmail.com",
+     *                          "email_verified_at": "2023-06-17T18:25:26.000000Z",
+     *                          "latitude": 0,
+     *                          "longitude": 0,
+     *                          "address": "1353 Dexter Motorway Suite 928",
+     *                          "profile_image": "user image",
+     *                          "created_at": "2023-06-17T18:25:26.000000Z",
+     *                          "updated_at": "2023-06-17T18:25:26.000000Z",
+     *                      })},
+     *     ),
+     *     @OA\Response(
+     *         response=404,
+     *         description="User not found"
+     *     )
+     * ) 
+     */
+
     public function show($id)
     {
         try {
@@ -54,6 +143,43 @@ class UserController extends Controller
             return anErrorOcurred();
         }
     }
+
+    /**
+     * Create a new user.
+     * Note: This endpoint does not work in swagger cause is not possible upload an image here.
+     * @OA\Post(
+     *      path="/api/users/",
+     *      summary="Create a new user",
+     *      tags={"Users"},
+     * 
+     *      @OA\RequestBody(
+     *          required=true,
+     *          @OA\JsonContent(
+     *          required={"name","email", "password","role_id"},
+     *          @OA\Property(property="id", type="integer", format="string"),
+     *          @OA\Property(property="name", type="string", format="string"),
+     *          @OA\Property(property="email", type="string", format="string" ),
+     *          @OA\Property(property="password", type="string", format="string" ),
+     *          @OA\Property(property="latitude", type="integer", format="string" ),
+     *          @OA\Property(property="longitude", type="integer", format="string" ),
+     *          @OA\Property(property="address", type="string", format="string" ),
+     *          @OA\Property(property="role_id", type="integer", format="string" ),
+     *                    ),
+     *              ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="User created successfully"  
+     *      ),
+     *      @OA\Response(
+     *          response=400,
+     *          description="bad request"  
+     *      ),
+     *      @OA\Response(
+     *          response="default",
+     *          description="An error has occurred"
+     *      )
+     * )
+     */
 
     public function store(Request $request)
     {
@@ -103,6 +229,48 @@ class UserController extends Controller
             return badRequestResponse400();
         }
     }
+
+    /**
+     * Update a user
+     * Note: This endpoint does not work in swagger if you add the field "image" cause is not possible upload an image here.
+     * @OA\Put(
+     *      path="/api/users/{id}",
+     *      summary="Update a user",
+     *      tags={"Users"},
+     *      @OA\Parameter(
+     *          description="id of user",
+     *          in="path",
+     *          name="id",
+     *          required=true,
+     *          @OA\Schema(type="string"),
+     *      ),
+     *      @OA\RequestBody(
+     *          required=true,
+     *          @OA\JsonContent(
+     *          @OA\Property(property="id", type="integer", format="string"),
+     *          @OA\Property(property="name", type="string", format="string"),
+     *          @OA\Property(property="email", type="string", format="string" ),
+     *          @OA\Property(property="password", type="string", format="string" ),
+     *          @OA\Property(property="latitude", type="integer", format="string" ),
+     *          @OA\Property(property="longitude", type="integer", format="string" ),
+     *          @OA\Property(property="address", type="string", format="string" ),
+     *          @OA\Property(property="role_id", type="integer", format="string" ),
+     *                    ),
+     *              ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="User updated successfully"  
+     *      ),
+     *      @OA\Response(
+     *          response=404,
+     *          description="User not found"
+     *      ),
+     *      @OA\Response(
+     *          response=400,
+     *          description="bad request"  
+     *      ),
+     * )
+     */
 
     public function update(Request $request, $id)
     {
@@ -163,6 +331,36 @@ class UserController extends Controller
             return badRequestResponse400();
         }
     }
+
+    /**
+     * Delete a user
+     * @OA\Delete(
+     *      path="/api/users/{id}",
+     *      summary="Delete a user",
+     *      tags={"Users"},
+     * 
+     *       @OA\Parameter(
+     *          description="id of user",
+     *          in="path",
+     *          name="id",
+     *          required=true,
+     *          @OA\Schema(type="string"),
+     *      ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="User delete succesfully"  
+     *      ),
+     *      @OA\Response(
+     *          response=404,
+     *          description="User not found"  
+     *      ),
+     *      @OA\Response(
+     *          response="default",
+     *          description="An Error ocurred"
+     *      )
+     * )
+     */
+
 
     public function delete($id)
     {
