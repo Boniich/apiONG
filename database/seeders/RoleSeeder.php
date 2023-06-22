f2f2fd0738a5918a0c00dfb8f6cb52575f2f82ec
boniich <ezequieldbo25@gmail.com>
2023-06-21 23:03:06 -0300

    feat: add permission to update roles

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
 
 class RoleSeeder extends Seeder
@@ -15,5 +16,7 @@ class RoleSeeder extends Seeder
     {
         $adminRole = Role::create(['id' => 1, 'name' => 'Admin']);
         $user = Role::create(['id' => 2, 'name' => 'User']);
+
+        Permission::create(['name' => 'roles.update'])->assignRole($adminRole);
     }
 }
