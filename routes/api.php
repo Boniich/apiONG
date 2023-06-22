70d547afe96617c90742076edaa748e4afe1c15c
boniich <ezequieldbo25@gmail.com>
2023-06-21 21:28:22 -0300

    refactor: remove a part of user url that is not necessary

--- routes/api.php
+++ routes/api.php
@@ -76,7 +76,7 @@ Route::get('roles/{id}', [RoleController::class, 'show']);
 Route::put('roles/{id}', [RoleController::class, 'update']);
 
 Route::get('users/{id}', [UserController::class, 'show']);
-Route::get('users/{search?}', [UserController::class, 'index']);
+Route::get('users/', [UserController::class, 'index']);
 
 Route::post('users', [UserController::class, 'store']);
 Route::put('users/{id}', [UserController::class, 'update']);
